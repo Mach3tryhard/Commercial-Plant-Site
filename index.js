4f459af3d121d28b17cb00d8bdc0d1e7b753f90e
Matei Sirghe <73358042+Mach3tryhard@users.noreply.github.com>
2026-03-16 15:48:57 +0200

    de facut chestia cu using fragments

--- index.js
+++ index.js
@@ -8,7 +8,23 @@ console.log("Folder index.js", __dirname);
 console.log("Folder curent (de lucru)", process.cwd());
 console.log("Cale fisier", __filename);
 
+app.get("/cale",function(req,res){
+    res.send("test")
+    console.log("Am primit o cerere GET pe /cale")
+});
 
+app.get("/",function(req,res){
+    res.sendFile(path.join(__dirname,"index.html"));
+
+});
+
+app.use("/resurse",express.static(path.join(__dirname,"resurse")));
+
+app.get("/cale2",function(req,res){
+    res.write("ceva\n");
+    res.write("altceva\n");
+    res.end();
+});
 
 app.listen(8080);
 console.log("Serverul a pornit!");
